85fc5a1a7b6451a58c72d4ffa934b80b6d8a0102
Nicolas Noël <33149687+Yacuzi@users.noreply.github.com>
2019-08-17 23:28:20 +0200

    test renommage niveau (tout casser?)

---
+++ GMTK_Game_Jam_2019_EA-NN/Assets/Scripts/Rename_Objects.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rename_Objects : MonoBehaviour
+{
+    public bool renameObjects;
+
+    private Transform[] allGameObjects;
+
+    void RenameMyObjects()
+    {
+        foreach (Transform anObject in allGameObjects)
+        {
+            int nbLevels = 0, nbExits = 0, nbEntries = 0;
+
+            if (anObject.CompareTag("Level"))
+            {
+                anObject.name = "Level (" + nbLevels + ")";
+                nbLevels++;
+            }
+
+            if(anObject.CompareTag("Exit"))
+            {
+                anObject.name = "Exit (" + nbExits + ")";
+                nbLevels++;
+            }
+
+            if(anObject.CompareTag("Entry"))
+            {
+                anObject.name = "Entry (" + nbEntries + ")";
+                nbLevels++;
+            }
+        }
+
+        renameObjects = false;
+    }
+
+    private void Start()
+    {
+        allGameObjects = GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (renameObjects)
+        {
+            RenameMyObjects();
+        }
+    }
+}
